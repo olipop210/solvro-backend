4192fd5d940489204650e092018af1b5cea5f20a
Oliwier Popielarczyk <71080644+olipop210@users.noreply.github.com>
2025-11-02 23:53:08 +0100

    code cleaning

--- src/prisma.service.ts
+++ src/prisma.service.ts
@@ -1,6 +1,8 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from 'generated/prisma';
 
+//Serwis Prisma do zarządzania połączeniem z bazą danych
+
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
   async onModuleInit() {
